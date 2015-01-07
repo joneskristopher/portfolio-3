e150083931f9a58a7b713146d83f60e5a4e5cb87
kristopher jones <grofernioken@gmail.com>
2015-01-07 14:02:42 -0800

    first commit

---
+++ public_html/script.js
@@ -0,0 +1,6 @@
+$("document").ready(function(){
+    $(".test").css("background-color", "grey")
+    $('a').css({"background-color": "black", "color": "white"});
+    $('p:first').css({"background-color": "green", "color": "white"});
+    $('p:last').css({"background-color": "red", "color": "black"});
+});
