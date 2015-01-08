8435d9994dde6d080f046be2311de02e62738e0f
kristopher jones <grofernioken@gmail.com>
2015-01-08 14:01:48 -0800

    hi

--- public_html/script.js
+++ public_html/script.js
@@ -3,4 +3,15 @@ $("document").ready(function(){
     $('a').css({"background-color": "black", "color": "white"});
     $('p:first').css({"background-color": "green", "color": "white"});
     $('p:last').css({"background-color": "red", "color": "black"});
+    $('#khazix').bind('click', alertButtonClick);
+//    $('#textbox1').bind('blur', onBlurEvent)
+//                  .bind('focus', onFocusEvent)
+//                  .bind('onmousedown', onMDownEvent)
+//                  .bind('onmouseup', onMUpEvent)
+//                  .bind('change', onChangeEvent);
 });
+
+function alertButtonClick() {
+    alert("there was a button clicked");
+}
+
